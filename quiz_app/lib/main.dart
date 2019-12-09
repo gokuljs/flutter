1cbacda8a7f56686dfa31d1392e6cd67e3a58a28
gokul js <51263906+gokuljs@users.noreply.github.com>
2019-12-09 18:09:00 +0530

    quiz app

--- quiz_app/lib/main.dart
+++ quiz_app/lib/main.dart
@@ -1,14 +1,31 @@
-import 'package:flutter/material.dart';// package in which  is used to import flutter dependency 
-// for example basic widgets like staefull widgets and stateless widget 
+import 'package:flutter/material.dart'; // package in which  is used to import flutter dependency
+// for example basic widgets like staefull widgets and stateless widget
 
-void main (){ // any program starts from main function
-runApp(Myapp());//runapp is the function given by the flutter two run the main widget 
-}
+//void main (){ // any program starts from main function
+//runApp(Myapp());//runapp is the function given by the flutter two run the main widget
+//}
 
-class Myapp extends StatelessWidget{
-  Widget build(BuildContext context) {// when ever u run the code the program calls the build method first and tries to put that on the screen 
+void main() => runApp(Myapp()); // other way of writing the above code
+// this can be used only if u have one app
+
+class Myapp extends StatelessWidget {
+  @override //on purpose we overriding the program again and again
+  // statelesswidget program slredy have a @override
+  Widget build(BuildContext context) {
+    // when ever u run the code the program calls the build method first and tries to put that on the screen
     // it should always return widget thats why ur using widget build
-    return MaterialApp(home :Text(" hello world")); // home is the one widgets in the flutter which shows the complete whitescreen
-  } 
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("quiz app"),
+          backgroundColor: Colors.black,
+        ),
+        body: Text("my quiz app"),
+        backgroundColor: Colors.orange,
+        
+      ),
+    ); // home is the one widgets in the flutter which shows the complete whitescreen
+  } // scaffold is one of the dependency present in the material dart package which prints out basic
+  //page design
 
-  }
+}
