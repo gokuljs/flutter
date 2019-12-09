e362a81537fbb7930baaad1cebf37e2092a93313
gokul js <51263906+gokuljs@users.noreply.github.com>
2019-12-09 18:44:00 +0530

    quiz app

--- quiz_app/lib/main.dart
+++ quiz_app/lib/main.dart
@@ -12,6 +12,12 @@ class Myapp extends StatelessWidget {
   @override //on purpose we overriding the program again and again
   // statelesswidget program slredy have a @override
   Widget build(BuildContext context) {
+    var questions = [
+      'what\'s your favourite color',
+      'what\'your favourite animal',
+      'whats your favourite car',
+      'whats your favourite game'
+    ];
     // when ever u run the code the program calls the build method first and tries to put that on the screen
     // it should always return widget thats why ur using widget build
     return MaterialApp(
@@ -20,9 +26,21 @@ class Myapp extends StatelessWidget {
           title: Text("quiz app"),
           backgroundColor: Colors.black,
         ),
-        body: Text("my quiz app"),
-        backgroundColor: Colors.orange,
-        
+        body: Column(
+          children: [
+            Text('the questions !'),
+            RaisedButton(child: Text('answer 1'),colorBrightness: Brightness.dark,onPressed: null,),
+            RaisedButton(child: Text('answer 2'),colorBrightness: Brightness.dark,onPressed: null,),
+            RaisedButton(child: Text('answer 3'),colorBrightness: Brightness.dark,onPressed: null,),
+            RaisedButton(child: Text('answer 4'),colorBrightness: Brightness.dark,onPressed: null,),
+            RaisedButton(child: Text('answer 5'),colorBrightness: Brightness.dark,onPressed: null,),
+
+
+
+          ],
+        ), // column widget are the invisible widgets which are used for styling
+        backgroundColor: Colors
+            .orange, // inside column widget there is childreen widget that can take list of widgets
       ),
     ); // home is the one widgets in the flutter which shows the complete whitescreen
   } // scaffold is one of the dependency present in the material dart package which prints out basic
