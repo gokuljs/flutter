d5d97d349e3aad33f01a58cec794c857f2e4bf3a
Gokul js <51263906+gokuljs@users.noreply.github.com>
2020-02-24 23:23:36 +0530

    flutter basics

--- test_app/lib/main.dart
+++ test_app/lib/main.dart
@@ -6,42 +6,65 @@ void main() {
   runApp(Myapp());
 }
 
-class Myapp extends StatelessWidget {
+class Myapp extends StatefulWidget {
+  @override
+  _MyappState createState() => _MyappState();
+}
+
+class _MyappState extends State<Myapp> {
   var questionindex=0;
 
   void answerquetion(){
-     questionindex=questionindex+1;
+    setState(() {
+           questionindex=questionindex+1;
+
+    });
+
     print(questionindex);
   }
+
   @override
   Widget build(BuildContext context) {
     var question = [
       'whats your favourite colour',
       'whats your favourite animal',
-      'whats your favourite car'
+      'whats your favourite car',
+      'whats your favourite caaxapr',
+      'whats your favourite caakmsmsr',
+      'whats your favourite caamsamsmar',
+
     ];
     return MaterialApp(
+      theme: ThemeData(
+        brightness: Brightness.dark,
+        primaryColor: Colors.lightBlue,
+        accentColor: Colors.cyan[600],
+      ),
       home: Scaffold(
+
         appBar: AppBar(
           title: Text('app1'),
         ), 
+        
         body: Column(
           children: [
             Text(question[questionindex]),
             RaisedButton(
-                          color: Colors.orange,
+                          color: Colors.redAccent,
                           child: Text('answer1'), 
-                          onPressed:answerquetion ),
+                          onPressed:answerquetion),
             RaisedButton(
-                          color: Colors.orange,
+                          color: Colors.redAccent,
+                        
                         child: Text('answer2'), 
                           onPressed: answerquetion,),
             RaisedButton(
-                          color: Colors.orange,
+                                                    color: Colors.redAccent,
+
                             child: Text('answer3'),
                            onPressed:answerquetion),
             RaisedButton( 
-                          color: Colors.orange,
+                          color: Colors.redAccent,
                           child: Text('answer4'),
                            onPressed: answerquetion,
             )
