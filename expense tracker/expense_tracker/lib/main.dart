858dfbce0fa43f3c1747db8a8221ea0099c2e32f
Gokul js <51263906+gokuljs@users.noreply.github.com>
2020-03-07 11:41:28 +0530

    expense tracker

--- expense tracker/expense_tracker/lib/main.dart
+++ expense tracker/expense_tracker/lib/main.dart
@@ -53,23 +53,10 @@ class Myhomepage extends StatelessWidget {
       mainAxisAlignment: MainAxisAlignment.spaceAround,
       crossAxisAlignment: CrossAxisAlignment.stretch,
           // main access and cross acces are the two major alignments which tells us how  the widget should be presitioned
-          children: <Widget>[
-                Container(
-                
-                  width: double.infinity,
-                  child: Card(
-                    // container is purely used for styling precesioning sizing widget u have
-                    // mainly used for aligingn and stuff 
-                  child:Text('chart'),
-                  color: Colors.blue,
-                  elevation: 9,
-
-                  ),
-                ),
-                Column(children: <Widget>[
-                  
-                ],)
-            ],
+         children: transactions.map((tx) {
+           return Card(child:Text(tx.title)
+           );
+         }).toList(),
           ),
     );
   }
