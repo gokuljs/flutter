63ce554f3c1e5e3eac45d2c6883cf3d3b80ebb86
Gokul js <51263906+gokuljs@users.noreply.github.com>
2020-03-16 02:45:49 +0530

    transaction.dart

--- new_app/expense/lib/widgets/new_transaction.dart
+++ new_app/expense/lib/widgets/new_transaction.dart
@@ -1,91 +1,14 @@
+// this widget is for controlling the text fields
 import 'package:flutter/material.dart';
-import 'package:intl/intl.dart';
-import './models/transaction.dart';
-import './widgets/transaction_list.dart';
 
-void main()=>runApp(Myapp());
-class Myapp extends StatelessWidget {
-  const Myapp({Key key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: "expense tracker",
-      home: Myhomepage(),
-    );
-  }
-}
-
-
-class Myhomepage extends StatelessWidget {
-    // final List<Transaction> transaction=[
-    //   Transaction(
-    //   id: 't1',
-    //   title: "New shoes",
-    //   amount:1700,
-    //   date: DateTime.now(),
-      
-    //   ),
-    //    Transaction(
-    //   id: 't2',
-    //   title: "New laptop",
-    //   amount:1700,
-    //   date: DateTime.now(),
-      
-    //   ),
-      
-    //    Transaction(
-    //   id: 't3',
-    //   title: "New lap",
-    //   amount:1700,
-    //   date: DateTime.now(),
-      
-    // //   ),
-      
-    //    Transaction(
-    //   id: 't3',
-    //   title: "New lap",
-    //   amount:1700,
-    //   date: DateTime.now(),
-      
-    //   ),
-    //    Transaction(
-    //   id: 't3',
-    //   title: "New lap",
-    //   amount:1700,
-    //   date: DateTime.now(),
-      
-    //   ),
-    //    Transaction(
-    //   id: 't3',
-    //   title: "New lap",
-    //   amount:1700,
-    //   date: DateTime.now(),
-      
-    //   ),
-    //  ]; 
-    // String titleinput;
-    // String amountinput;
-
-    final titlecontroller=TextEditingController();
+class Newtransaction extends StatelessWidget {
+      final titlecontroller=TextEditingController();
     final amountcontroller=TextEditingController();
 
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-
-      appBar: AppBar(
-        title: Text('expense tracker'),
-      ),
-
-      body:Column(
-       // mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-      children: <Widget>[
-        
-
-
+    return 
         Card(
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.end,
@@ -144,10 +67,6 @@ class Myhomepage extends StatelessWidget {
                )
            ],
           ),
-        ),
-       ],)
-
-    );
-    
+        );
   }
 }
